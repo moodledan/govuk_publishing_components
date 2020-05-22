bd1e50e858385575aaf0ff14e26464b30e31b2f5
Ian James <ian.james@digital.cabinet-office.gov.uk>
2020-05-22 17:18:11 +0100

    Fix linting errors

--- spec/visual_regression_tests/all_components.rb
+++ spec/visual_regression_tests/all_components.rb
@@ -7,22 +7,20 @@ describe "visual regression test runner Percy", type: :feature, js: true do
 
     component_links = []
 
-    find("#components-in-the-gem-list").all("a").each { |link|
+    find("#components-in-the-gem-list").all("a").each do |link|
       component_links.push link[:href]
-    }
+    end
 
     # component_links.first(2).last(1).each{ |link|
-    component_links.each { |link|
+    component_links.each do |link|
       slug = link.gsub(/http:\/\/127.0.0.1:\d{4,5}\/component-guide\//, "")
 
-      unless slug == "govspeak"
-        puts "Visiting #{link}"
-        visit "#{link}/preview"
-        name = title.gsub(/(: Default|) preview - Component Guide/, "")
-        page.find(:css, '#wrapper', wait: 10)
-        # sleep 1
-        Percy.snapshot(page, name: name)
-      end
-    }
+      next if slug == "govspeak" # skip Govspeak because it's just too long
+
+      visit "#{link}/preview"
+      name = title.gsub(/(: Default|) preview - Component Guide/, "")
+      page.find(:css, "#wrapper", wait: 10)
+      Percy.snapshot(page, name: name)
+    end
   end
 end
