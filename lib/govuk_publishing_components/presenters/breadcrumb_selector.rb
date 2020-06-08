a515232f17bb8f60fff60d74023944e4f89e2a3a
Rob Nichols <rob.nichols@digital.cabinet-office.gov.uk>
2020-06-08 17:23:36 +0100

    341 Refactor in preparation for adding topic breadcrumbs
    Rename ContentBreadcrumbsBasedOnParent to ContentBreadcrumbsBasedOnAncestors
    to clarify when working directly with parent and when with a group of parents
    in context navigation. Also removed breadcrumb outer hash as redundant.
    
    Separated finder_breadcrumbs from default breadcrumbs to simplify logic

--- lib/govuk_publishing_components/presenters/breadcrumb_selector.rb
+++ lib/govuk_publishing_components/presenters/breadcrumb_selector.rb
@@ -45,15 +45,17 @@ module GovukPublishingComponents
         }
       end
 
+      def parent_is_step_by_step?
+        parent_item_options[:step_by_step]
+      end
+
       def output
         return content_item_options unless content_item_navigation.html_publication_with_parent?
         return parent_item_options if parent_item_navigation.priority_breadcrumbs
 
-        step_by_step_header = parent_item_options[:step_by_step]
-
         {
-          step_by_step: step_by_step_header,
-          breadcrumbs: step_by_step_header ? parent_breadcrumbs.first : parent_breadcrumbs,
+          step_by_step: parent_is_step_by_step?,
+          breadcrumbs: parent_is_step_by_step? ? parent_breadcrumbs.first : parent_breadcrumbs,
         }
       end
 
@@ -61,7 +63,7 @@ module GovukPublishingComponents
         if navigation.content_tagged_to_a_finder?
           {
             step_by_step: false,
-            breadcrumbs: navigation.breadcrumbs,
+            breadcrumbs: navigation.finder_breadcrumbs,
           }
         elsif navigation.content_tagged_to_current_step_by_step?
           {
